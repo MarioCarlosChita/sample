1a6534a5c107460c1af44bd7bc828eb89e61dfd5
Mario Carlos <mariochita1996@gmail.com>
2024-11-21 11:38:37 +0000

    fix(globe port listener issue): add a port script to be listen in globe.dev

--- bin/server.dart
+++ bin/server.dart
@@ -1,6 +1,8 @@
 import 'package:vania/vania.dart';
 import 'package:vania_sample/config/app.dart';
+import 'dart:io';
 
-void main() async {
+void main() async { 
+  final port  = Platform.environment['PORT'] ?? '8080';
   Application().initialize(config: config);
 }
